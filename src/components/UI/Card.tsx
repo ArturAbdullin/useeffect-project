075f995fac06784fd4c3eafa4362676c1fe4dceb
Artur Abdullin <artur.abdullin@gmail.com>
2022-08-11 22:01:24 +0300

    feat: add Card component

---
+++ src/components/UI/Card.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+import styles from "./Card.module.css";
+
+type CardProps = {
+  children?: React.ReactNode;
+  className?: string;
+};
+
+const Card: FC<CardProps> = (props) => {
+  return (
+    <div className={`${styles.card} ${props.className}`}>{props.children}</div>
+  );
+};
+
+export default Card;
