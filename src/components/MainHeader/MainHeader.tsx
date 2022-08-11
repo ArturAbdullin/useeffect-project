e90b914457e41ca29356bd1b979b1a639a7ca70b
Artur Abdullin <artur.abdullin@gmail.com>
2022-08-11 15:37:50 +0300

    feat: add MainHeader component

---
+++ src/components/MainHeader/MainHeader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./MainHeader.module.css";
+
+const MainHeader = () => {
+  return (
+    <header className={styles['main-header']}>
+      <h1>A Typical Page</h1>
+    </header>
+  );
+};
+export default MainHeader;
