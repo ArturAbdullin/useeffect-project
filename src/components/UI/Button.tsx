dc61086c6d8fd1591335d9f59fd43ef205a7f47d
Artur Abdullin <artur.abdullin@gmail.com>
2022-08-11 23:53:11 +0300

    feat: add Button component

---
+++ src/components/UI/Button.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from "react";
+import styles from "./Button.module.css";
+
+type ButtonProps = {
+  children?: React.ReactNode;
+  className?: string;
+  disabled?: boolean;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  type?: "button" | "submit";
+};
+
+const Button: FC<ButtonProps> = ({
+  children,
+  className = "",
+  disabled = false,
+  onClick,
+  type = "button",
+}) => {
+  return (
+    <button
+      type={type}
+      className={`${styles.button} ${className}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
