a44a79738a29cb0758a8078016d9bc5367e97589
Artur Abdullin <artur.abdullin@gmail.com>
2022-08-12 18:31:11 +0300

    feat(App): add main section with login process

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import MainHeader from "./components/MainHeader/MainHeader";
+import { LoginEventHandler } from "./models/eventHandlers";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  const loginHandler: LoginEventHandler = (email, password) => {
+    setIsLoggedIn(true);
+  };
+
   return (
-    <></>
-  )
+    <>
+      <MainHeader isLoggedIn={isLoggedIn} onLogout={() => {}} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home />}
+      </main>
+    </>
+  );
 }
 
 export default App;
