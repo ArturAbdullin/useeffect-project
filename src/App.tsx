d1ed18034fd08f7c2c3827fa6e3af3d4774e98f1
ArturAbdullin <artur.abdullin@gmail.com>
2022-08-15 19:58:12 +0300

    feat(App): add useEffect hook to use logging state data from local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import MainHeader from "./components/MainHeader/MainHeader";
@@ -7,13 +7,20 @@ import { LoginEventHandler, LogoutEventHandler } from "./models/eventHandlers";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
+  useEffect(() => {
+    const loggingState = localStorage.getItem("isLoggedIn");
+    if (loggingState === "1") setIsLoggedIn(true);
+  }, []);
+
   const loginHandler: LoginEventHandler = (email, password) => {
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler: LogoutEventHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
-  }
+  };
 
   return (
     <>
