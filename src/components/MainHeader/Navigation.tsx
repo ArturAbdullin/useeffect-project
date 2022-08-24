d82bd8cb13f98a203cd8321776fd8a5db0a51a0d
Artur Abdullin <artur.abdullin@gmail.com>
2022-08-24 22:55:57 +0300

    feat: use Context API, useContext hook approach

--- src/components/MainHeader/Navigation.tsx
+++ src/components/MainHeader/Navigation.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useContext } from "react";
 import AuthContext from "../../contexts/auth-context";
 import { AuthenticationProps } from "../../models/AuthenticationProps";
 import styles from "./Navigation.module.css";
@@ -6,30 +6,25 @@ import styles from "./Navigation.module.css";
 type NavigationProps = {} & AuthenticationProps;
 
 const Navigation: FC<NavigationProps> = (props) => {
+  const ctx = useContext(AuthContext);
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
-          <nav className={styles.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <>
-                  <li>
-                    <a href="/">Users</a>
-                  </li>
-                  <li>
-                    <a href="/">Admin</a>
-                  </li>
-                  <li>
-                    <button onClick={props.onLogout}>Logout</button>
-                  </li>
-                </>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={styles.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <>
+            <li>
+              <a href="/">Users</a>
+            </li>
+            <li>
+              <a href="/">Admin</a>
+            </li>
+            <li>
+              <button onClick={props.onLogout}>Logout</button>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
   );
 };
 
